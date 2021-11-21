250a1be6147513722eb00b9562a6b36eca16e0bd
blockdoyle <blockdoyle@gmail.com>
2021-11-21 16:58:50 -0500

    Finished Module

--- hashCheck.py
+++ hashCheck.py
@@ -8,7 +8,8 @@ def getContent(file):
     return content
 
 def write2File(file, alg, hashValue):
-    openFile = open(f'.\hashes\{file}.hash', "w")
+    openFileBasename = os.path.basename(file)
+    openFile = open(f'.\hashes\{openFileBasename}.hash', "w")
     openFile.write(alg + ":" + hashValue)
     openFile.close()
     
@@ -30,18 +31,18 @@ def compareHash(file,alg):
     else:
         return "Hash does not match stored value."
 
-inFile = input("Enter file path for hash value: ")
-algSelect = input("Enter the algorithm you would like to use from below list:\n\n"+str(hashlib.algorithms_available)+"\n")
-hashResult = fileHash(inFile, algSelect)
+# inFile = input("Enter file path for hash value: ")
+# algSelect = input("Enter the algorithm you would like to use from below list:\n\n"+str(hashlib.algorithms_available)+"\n")
+# hashResult = fileHash(inFile, algSelect)
 
-writeOrCompare = input("Would you like to write or compare? ")
-match writeOrCompare:
-    case "write":
-        conOrFile = input("Would you like to write to console, or file? ")
-        match conOrFile:
-            case "console":
-                print(inFile + ":" + hashResult)
-            case "file":
-                write2File(inFile,algSelect,hashResult)
-    case "compare":
-        print(compareHash(inFile,algSelect))
+# writeOrCompare = input("Would you like to write or compare? ")
+# match writeOrCompare:
+#     case "write":
+#         conOrFile = input("Would you like to write to console, or file? ")
+#         match conOrFile:
+#             case "console":
+#                 print(inFile + ":" + hashResult)
+#             case "file":
+#                 write2File(inFile,algSelect,hashResult)
+#     case "compare":
+#         print(compareHash(inFile,algSelect))
