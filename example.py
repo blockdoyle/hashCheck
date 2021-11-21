25336ec2d9616b27d2d32a68e8210aaf0e8b9468
blockdoyle <blockdoyle@gmail.com>
2021-11-21 17:06:30 -0500

    added comments to example.py

--- example.py
+++ example.py
@@ -1,17 +1,17 @@
-import hashCheck
+import hashCheck # imports hashCheck.py
 
-inFile = input("Enter file path for hash value: ")
-algSelect = input("Enter the algorithm you would like to use from below list:\n\n"+str(hashCheck.hashlib.algorithms_available)+"\n")
-hashResult = hashCheck.fileHash(inFile, algSelect)
-print(hashResult)
-writeOrCompare = input("Would you like to write or compare? ")
+inFile = input("Enter file path for hash value: ") # asks the user for file location
+algSelect = input("Enter the algorithm you would like to use from below list:\n\n"+str(hashCheck.hashlib.algorithms_available)+"\n") # asks user to select algorithm from displayed list
+hashResult = hashCheck.fileHash(inFile, algSelect) # stores return value from hashCheck.fileHash(inFile, algSelect)
+
+writeOrCompare = input("Would you like to write or compare? ") #asks user if they would like to write or compare hash
 match writeOrCompare:
     case "write":
-        conOrFile = input("Would you like to write to console, or file? ")
+        conOrFile = input("Would you like to write to console, or file? ") # asks user if they would like to write to console or to file
         match conOrFile:
             case "console":
-                print(inFile + ":" + hashResult)
+                print(inFile + ":" + algSelect + ":" + hashResult) # writes filename, algorithm, and hashResult to console
             case "file":
-                hashCheck.write2File(inFile,algSelect,hashResult)
+                hashCheck.write2File(inFile,algSelect,hashResult) # writes filename, algorithm, and hashResult to file
     case "compare":
-        print(hashCheck.compareHash(inFile,algSelect))
+        print(hashCheck.compareHash(inFile,algSelect)) # compares saved *.hash file with current file hash
