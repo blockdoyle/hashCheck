cc1d3dc6ad3b98615523beb030f4e63cfbb1f6ce
blockdoyle <blockdoyle@gmail.com>
2021-11-21 16:59:14 -0500

    removed test comments

--- hashCheck.py
+++ hashCheck.py
@@ -30,19 +30,3 @@ def compareHash(file,alg):
         return "Hash matches stored value."
     else:
         return "Hash does not match stored value."
-
-# inFile = input("Enter file path for hash value: ")
-# algSelect = input("Enter the algorithm you would like to use from below list:\n\n"+str(hashlib.algorithms_available)+"\n")
-# hashResult = fileHash(inFile, algSelect)
-
-# writeOrCompare = input("Would you like to write or compare? ")
-# match writeOrCompare:
-#     case "write":
-#         conOrFile = input("Would you like to write to console, or file? ")
-#         match conOrFile:
-#             case "console":
-#                 print(inFile + ":" + hashResult)
-#             case "file":
-#                 write2File(inFile,algSelect,hashResult)
-#     case "compare":
-#         print(compareHash(inFile,algSelect))
